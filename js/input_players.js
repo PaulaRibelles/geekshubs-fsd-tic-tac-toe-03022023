203d25db4118ebfbb48f016a1d06b3e7eef790da
paula <paula_ribe93@hotmail.com>
2023-02-09 10:05:45 +0100

    Vinculamos el boton Start a la página del tablero y modificamos el js para que los nombres de los inputs se muestren en la página board

--- js/input_players.js
+++ js/input_players.js
@@ -1,34 +1,23 @@
+
+
+
 let playerInputs = {
     player1 : '',
-    plaer2 : ''
+    player2 : ''
 };
+
 
 let inputsCapturados = Array.from(document.getElementsByTagName("input"));
-
-const save = () => {
-    sessionStorage.setItem("datosUsuarios", JSON.stringify(playerInputs));
-};
-
-const read = () => {
-    let datosDeSesion = JSON.parse(sessionStorage.getItem("datosUsuarios"));
-
-    console.log(datosDeSesion);
-};
-
 inputsCapturados.map(
     (inputNamePlayer) => {
         inputNamePlayer.addEventListener("input", ()=> {
             //Esta funcion se ejecuta cada vez que alguien escribe en un input.
-            for(let elementoQueRecorro in playerInputs){
-                if(inputNamePlayer.name === elementoQueRecorro){
+            for (let elementoQueRecorro in playerInputs){
+                if(inputNamePlayer.name == elementoQueRecorro){
                     playerInputs[elementoQueRecorro] = inputNamePlayer.value;
+                    sessionStorage.setItem(inputNamePlayer.name, inputNamePlayer.value);
                 };
             };
         });
     }
 );
-
-function playersnames(){
-    document.getElementsByTagName('input').value;
-    document.getElementsByTagName('playersnames').innerHTML = userInput;
-};
--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 
+// Recuperar valores de los nombres
+
+let namePlayer1 = sessionStorage.getItem("player1");
+let namePlayer2 = sessionStorage.getItem("player2");
+
+document.write(namePlayer1);
+document.write(namePlayer2);
+
+
 // BOARD
 
 // Convertimos en Array
