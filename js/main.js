057096f6258eb37e53e90dd70a3a6104d06c16d3
paula <paula_ribe93@hotmail.com>
2023-02-10 14:58:08 +0100

    Creamos la función que detecte la combinación ganadora y nos envíe a la página winner

--- js/main.js
+++ js/main.js
@@ -4,11 +4,8 @@
 let namePlayer1 = sessionStorage.getItem("player1");
 let namePlayer2 = sessionStorage.getItem("player2");
 
-document.write(namePlayer1);
-document.write(namePlayer2);
-
-player1.innerHTML = `${namePlayer1.player1}`;
-player2.innerHTML = `${namePlayer2.player2}`;
+document.getElementById("player_1").innerHTML = `<p>${namePlayer1}<p/>`;
+document.getElementById("player_2").innerHTML = `<p>${namePlayer2}<p/>`;
 
 // BOARD
 
@@ -43,7 +40,26 @@ let optionsWinner = [
 // Comprobamos por cada movimiento si hay un ganador
 
 const checkWinner = () => {
-    console.log(myBoard);
+    if (myBoard[0] == myBoard[1] && myBoard[1] == myBoard[2]){
+        
+    window.location.href = "../pages/winner.html"
+    }else if (myBoard[3] == myBoard[4] && myBoard[4] == myBoard[5]){
+        window.location.href = "../pages/winner.html"
+    }else if (myBoard[6] == myBoard[7] && myBoard[7] == myBoard[8]){
+        window.location.href = "../pages/winner.html"
+    }else if (myBoard[0] == myBoard[3] && myBoard[3] == myBoard[6]){
+        window.location.href = "../pages/winner.html"
+    }else if (myBoard[1] == myBoard[4] && myBoard[4] == myBoard[7]){
+        window.location.href = "../pages/winner.html"
+    }else if (myBoard[2] == myBoard[5] && myBoard[5] == myBoard[8]){
+        window.location.href = "../pages/winner.html"
+    }else if (myBoard[0] == myBoard[4] && myBoard[4] == myBoard[8]){
+        window.location.href = "../pages/winner.html"
+    }else if (myBoard[2] == myBoard[4] && myBoard[4] == myBoard[6]){
+        window.location.href = "../pages/winner.html"
+    }else {
+        return false;
+    }
 };
 
 // Pintamos X || O según el turno del jugador
@@ -53,58 +69,14 @@ board.map(
         box.addEventListener('click', ()=> {
             if((box.innerHTML === "") && (turnP1 > 0 || turnP2 > 0)){
                 box.innerHTML = (turn) ? "O" : "X";
-
-                (turn) ? turnP1-- : turnP2--;
-
                 myBoard[box.id] = (turn) ? "O" : "X";
-
-                checkWinner();
-
-                //Cambiamos de turno
-                turn = !turn;
+                (turn) ? turnP1-- : turnP2--;
+                if (checkWinner()) {
+                    console.log(myBoard[box.id]);
+                }
+                turn = !turn;          //Cambiamos de turno
             };
         });
     }
 );
 
-// Declaramos una función que nos indique qué jugada ha ganado
-
-function gameOver(){
-    gameBoard.winner = '?';
-    return winner(0,1,2)
-        || winner(3,4,5)
-        || winner(6,7,8)
-        || winner(0,3,6)
-        || winner(1,4,7)
-        || winner(2,5,8)
-        || winner(0,4,8)
-        || winner(6,4,2)
-        || stalemate();
-}
-
-function winner(p1, p2, p3){
-    let s = board.boardString;
-    let c1 = s.charAt(p1);
-
-    if(c1 == '-') return false;
-
-    let c2 = s.charAt(p2);
-
-    if (c1 !=c2) return false;
-
-    let c3 = s.charAt(p3);
-
-    if (c1 != c3) return false;
-
-    board.winner = c1;
-    return true;
-}
-
-function stalemate(){
-    let s = board.boardString;
-    for (var i=0; i<9; i++){
-        if(s.charAt(i) == '-') return false;
-    }
-    board.winner = "C";
-    return true;
-}
